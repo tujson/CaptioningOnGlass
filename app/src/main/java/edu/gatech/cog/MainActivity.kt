a4642ef8244aff920b99ae0c58e44758b6da9b8e
Jason Tu <title.kubo@gmail.com>
2019-10-16 12:13:41 -0400

    finished integration with cmusphinx

--- app/src/main/java/edu/gatech/cog/MainActivity.kt
+++ app/src/main/java/edu/gatech/cog/MainActivity.kt
@@ -1,69 +1,74 @@
 package edu.gatech.cog
 
 import android.app.Activity
-import android.content.Intent
-import android.media.AudioFormat
-import android.media.AudioRecord
-import android.media.MediaRecorder
 import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
 import android.util.Log
-import android.widget.RemoteViews
-import com.google.android.glass.timeline.LiveCard
+import android.widget.TextView
+import edu.cmu.pocketsphinx.*
+import java.io.File
+import java.lang.Exception
 
 class MainActivity : Activity() {
-    private lateinit var speechRecognizer: SpeechRecognizer
+
+    private val DIGITS_SEARCH = "digits"
+    private val PHONE_SEARCH = "phones"
+    private var speechRecognizer: SpeechRecognizer? = null
 
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         setContentView(R.layout.activity_main)
 
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-        speechRecognizer.setRecognitionListener(Listener())
+        val assets = Assets(this)
+        val assetsDir = assets.syncAssets()
+        setupRecognizer(assetsDir)
 
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+    }
 
-        speechRecognizer.startListening(intent)
+    private fun setupRecognizer(assetsDir: File) {
+        speechRecognizer = SpeechRecognizerSetup.defaultSetup()
+                .setAcousticModel(File(assetsDir, "en-us-ptm"))
+                .setDictionary(File(assetsDir, "cmudict-en-us.dict"))
+                .recognizer
+
+        speechRecognizer?.addGrammarSearch(DIGITS_SEARCH, File(assetsDir, "digits.gram"))
+        speechRecognizer?.addAllphoneSearch(PHONE_SEARCH, File(assetsDir, "en-phone.dmp"))
+
+        speechRecognizer?.addListener(object: RecognitionListener{
+            override fun onResult(p0: Hypothesis?) {
+                Log.v("MainActivity", "Result ${p0?.hypstr}")
+            }
+
+            override fun onPartialResult(p0: Hypothesis?) {
+                Log.v("MainActivity", "Partial ${p0?.hypstr}")
+                runOnUiThread {
+                    (findViewById(R.id.tvText) as TextView).text = p0?.hypstr
+                }
+            }
+
+            override fun onTimeout() {
+                Log.v("MainActivity", "Timeout")
+            }
+
+            override fun onBeginningOfSpeech() {
+                Log.v("MainActivity", "BeginningOfSpeech")
+            }
+
+            override fun onEndOfSpeech() {
+                Log.v("MainActivity", "EndOfSpeech")
+            }
+
+            override fun onError(p0: Exception?) {
+                Log.e("MainActivity", "speechRecognizer", p0)
+            }
+
+        })
+
+        speechRecognizer?.startListening("digits")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        speechRecognizer.destroy()
-    }
-
-    class Listener : RecognitionListener {
-        override fun onReadyForSpeech(params: Bundle?) {
-        }
-
-        override fun onRmsChanged(rmsdB: Float) {
-        }
-
-        override fun onBufferReceived(buffer: ByteArray?) {
-        }
-
-        override fun onPartialResults(partialResults: Bundle?) {
-        }
-
-        override fun onEvent(eventType: Int, params: Bundle?) {
-        }
-
-        override fun onBeginningOfSpeech() {
-        }
-
-        override fun onEndOfSpeech() {
-        }
-
-        override fun onError(error: Int) {
-        }
-
-        override fun onResults(results: Bundle?) {
-            val data = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-            data?.forEach {
-                Log.v("MainActivity", "onResults: $it")
-            }
-        }
+        speechRecognizer?.cancel()
+        speechRecognizer?.shutdown()
     }
 }
